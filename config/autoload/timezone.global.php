b02974e0427ffe4d25c466f682e4d497e1377207
Puskás Zsolt <errotan@gmail.com>
2018-12-30 11:35:34 +0100

    set timezone

---
+++ config/autoload/timezone.global.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Puskás Zsolt <errotan@gmail.com>
+ * Licensed under the MIT license.
+ */
+
+return [
+    'timezone' => 'Europe/Budapest',
+];
--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -7,10 +7,28 @@
 
 namespace Application;
 
+use Zend\EventManager\EventInterface;
+
+/**
+ * Application setup class.
+ */
 class Module
 {
+    /**
+     * @return array
+     */
     public function getConfig()
     {
-        return include __DIR__ . '/../config/module.config.php';
+        return include __DIR__.'/../config/module.config.php';
+    }
+
+    /**
+     * @param EventInterface $event bootstrap event holder
+     *
+     * @return void
+     */
+    public function onBootstrap(EventInterface $event)
+    {
+        date_default_timezone_set($event->getApplication()->getServiceManager()->get('config')['timezone']);
     }
 }
